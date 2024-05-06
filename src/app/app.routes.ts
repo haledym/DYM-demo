f1118204fe51db972cfdbf0a359c681f02e241b8
hale <ha-l@dymvietnam.net>
2024-05-06 16:36:19 +0700

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainpageComponent } from './mainpage/mainpage.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: '/homepage', pathMatch: 'full' },
-    { path: 'heroes', component: MainpageComponent }
-];
